6d7e18e1a791817375548c4f386331092803bba6
Miach-A <ovmiach@gmail.com>
2023-02-13 22:46:33 +0200

    Add HostApplicationLifetime in worker

--- RadencyDataProcessing/Worker.cs
+++ RadencyDataProcessing/Worker.cs
@@ -3,19 +3,35 @@ namespace RadencyDataProcessing
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(
+            ILogger<Worker> logger,
+            IHostApplicationLifetime hostApplicationLifetime)
         {
             _logger = logger;
+            _hostApplicationLifetime = hostApplicationLifetime;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await Task.Delay(1000, stoppingToken);
+                }
             }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(string.Concat("Unhandling exception. ", ex.Message));
+            }
+            finally
+            {
+                _hostApplicationLifetime.StopApplication();
+            }
+
         }
     }
 }
